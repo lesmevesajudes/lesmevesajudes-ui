8859f1de347e466a932d45d7423f51ccfeb8708e
jvalduvieco <jvalduvieco@gmail.com>
2018-02-21 17:28:07 +0100

    tweak styles and logos

--- src/indexPage/IndexPage.js
+++ src/indexPage/IndexPage.js
@@ -11,8 +11,7 @@ class IndexPage extends React.Component {
                         <div className='AppLogo'/>
                     </div>
                     <div className='CTA'>
-                        <b>Vols saber a quins ajuts públics pots optar?</b>
-                        <p className="PresentationText"> Aquesta eina et permetrà consultar a quins ajuts i prestacions
+                        <p className="PresentationText"><b>Vols saber a quins ajuts públics pots optar?</b> Aquesta eina et permetrà consultar a quins ajuts i prestacions
                             socials tens dret de manera àgil i precisa. Podràs trobar-hi ajudes gestionades per
                             l’Ajuntament, la Generalitat i l’Estat. El simulador, que es troba en fase inicial, anirà
                             incorporant nous ajuts.
