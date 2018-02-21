7b0255f0c6b321e2f3f6ed4ee6b7a390f8593db9
jvalduvieco <jvalduvieco@gmail.com>
2018-02-21 12:41:42 +0100

    tweak headers

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -5,10 +5,12 @@ import Link from "react-router-dom/es/Link";
 class AppHeader extends Component {
     render() {
         return (
-            <div className="AppHeader">
-                <Link to="/">
-                    <div className="AppLogoPetit"/>
-                </Link>
+            <div className="AppHeaderContainer">
+                <div className="AppHeader">
+                    <Link to="/">
+                        <div className="AppLogoPetit"/>
+                    </Link>
+                </div>
             </div>);
     }
 }
