912faae59388be1f692d2f49cb18841052364188
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-04-04 11:02:52 +0200

    Fix mistakes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_SECRET_CODE || "http://localhost:2000";
+const API_URL = process.env.REACT_API_URL || "http://localhost:2000";
 const ENVIRONMENT = process.env.NODE_ENV || "development";
 
 export { API_URL, ENVIRONMENT };
