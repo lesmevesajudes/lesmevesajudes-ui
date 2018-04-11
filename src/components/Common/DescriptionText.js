2df7616ce060c843b1450805840a3aa36fb99a25
jvalduvieco <jvalduvieco@gmail.com>
2018-04-11 11:31:19 +0200

    identation

--- src/components/Common/DescriptionText.js
+++ src/components/Common/DescriptionText.js
@@ -2,23 +2,23 @@ import React, {Component} from 'react';
 import {Grid} from 'material-ui'
 
 class DescriptionText extends Component {
-    render() {
-        return (
-            <Grid item>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-							quis gravida erat. Donec ac nulla et ligula facilisis auctor sed
-							eu enim. In hac habitasse platea dictumst. In ultrices rhoncus
-							felis faucibus blandit. Curabitur scelerisque, urna nec finibus
-							molestie, orci est imperdiet arcu, et fermentum velit est nec
-							nisi. Morbi mollis vel purus sed fringilla. Aliquam ut tellus at
-							justo varius vulputate non in justo. Ut at lorem at augue
-							facilisis varius. Aliquam semper quam a dolor porttitor, et
-							condimentum velit malesuada. In ac diam quis dolor lacinia
-							varius ut sit amet ligula. Nulla facilisi. Integer vestibulum
-							neque ac sagittis consequat. Morbi non odio lorem.
-            </Grid>
-        );
-    }
+  render() {
+    return (
+        <Grid item>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          quis gravida erat. Donec ac nulla et ligula facilisis auctor sed
+          eu enim. In hac habitasse platea dictumst. In ultrices rhoncus
+          felis faucibus blandit. Curabitur scelerisque, urna nec finibus
+          molestie, orci est imperdiet arcu, et fermentum velit est nec
+          nisi. Morbi mollis vel purus sed fringilla. Aliquam ut tellus at
+          justo varius vulputate non in justo. Ut at lorem at augue
+          facilisis varius. Aliquam semper quam a dolor porttitor, et
+          condimentum velit malesuada. In ac diam quis dolor lacinia
+          varius ut sit amet ligula. Nulla facilisi. Integer vestibulum
+          neque ac sagittis consequat. Morbi non odio lorem.
+        </Grid>
+    );
+  }
 }
 
 export default DescriptionText;
