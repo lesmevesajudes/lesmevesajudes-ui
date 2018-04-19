3c5a5f57433ab7161c5c3180695eef6d38183100
Joan Serrano <john.voiden@gmail.com>
2018-04-19 12:47:14 +0200

    Only left a few commands!

--- tests/browser/Pages/AddParent.js
+++ tests/browser/Pages/AddParent.js
@@ -13,7 +13,9 @@ function setPadro(padro){
 function percentageDisability(percentage){
     return this.setValue("@disability", percentage)
 }
-
+function rawIncomeActual(money){
+  return this.setValue("@")
+}
 module.exports = {
   url: function() { 
 		return this.api.launchUrl; 
@@ -33,15 +35,24 @@ module.exports = {
       identityPassport: '[data-test="passport"]',
       laboralSituation: '[data-test="situacio_laboral"]',
       unemployed: '[data-test="desocupat"]',
-      disability: '"input[name="grau_discapacitat"]',
+      disability: 'input[name="grau_discapacitat"]',
+      rawIncome: 'input[name="ingressos_bruts"]',
       genderViolence: 'input[name="victima_violencia_de_genere"]',
       householdViolence:  'input[name="victima_violencia_domestica"]',
+      joinedJail: 'input[name="ingressat_en_centre_penitenciari"]',
+      joinedJailCanWork: 'input[name="ingressat_en_centre_penitenciari_pot_treballar"]',
       divorced: 'input[name="es_divorciada_de_familia_reagrupada"]',
       totalOrphan: 'input[name="es_divorciada_de_familia_reagrupada"]',
       worked6MonthsAbroad: 'input[name="ha_treballat_a_l_estranger_6_mesos"]',
+      offWorkVolunteer12Months: 'input[name="en_els_ultims_12_mesos_ha_fet_baixa_voluntaria_de_la_feina"]',
+      raiBenefit12Months: 'input[name="ha_estat_beneficiari_de_la_rai_en_els_ultims_12_mesos"]',
+      before3RaiBefore: 'input[name="ha_estat_beneficiari_de_les_tres_rai_anteriors"]'
       worked6MonthsAbroadReturn12LastMonth: 'input[name="ha_treballat_a_l_estranger_6_mesos_i_ha_retornat_en_els_ultims_12_mesos"]',
       finishedBenefitForUnemployed: 'input[name="ha_esgotat_prestacio_de_desocupacio"]',
-      occupationApplicantinput: '[name="inscrit_com_a_demandant_docupacio"]'
+      occupationApplicant: 'input[name="inscrit_com_a_demandant_docupacio"]',
+      occupationApplicant12Month: 'input[name="demandant_d_ocupacio_durant_12_mesos"]',
+      recentSearchingOfWork: 'input[name="durant_el_mes_anterior_ha_presentat_solicituds_recerca_de_feina"]',
+      
   },
   commands: [{
       elSeuNom: setName,
@@ -82,14 +93,45 @@ module.exports = {
         .waitForElementVisible("@identityPassport")
         .click("@identityPassport")
       },
-      Minusvalia: percentageDisability,
+      IngresosBruts: rawIncomeActual,
+      minusvaliaPercentatge: percentageDisability,
+      IngeresatACentrePeninteciari: () => {
+        return this.click("@joinedJail")
+      },
+      IngeresatACentrePeninteciari: () => {
+        return this.click("@joinedJailCanWork")
+      },
       victimaDeViolenciaDeGenera: () => {
        return this.click("@genderViolence") 
       },
+      
       victimaDeViolenciaDomestica: () =>{
         return this.click("@householdViolence")
+      },
+      divorciadaFamiliaRegrupada: () => {
+        return this.click("@divorced")
+      },
+      orfeProgenitos: () => {
+        return this.click("@totalOrphan")
+      },
+      treballatAlExtranger6Mesos: () => {
+        return this.click("@worked6MonthsAbroad")
+      },
+      treballatAlExtranger6MesosRetornatUltims12Mesos: () => {
+        return this.click("@worked6MonthsAbroadReturn12LastMonth")
+      },
+      esgotatPrestacioDesocupacio: () => {
+        return this.click("@finishedBenefitForUnemployed")
+      },
+      DemandantDesocupacio: () => {
+        return this.click("2occupationApplicant")
+      },
+      DemandantDesocupacioDurant12Mesos: () => {
+        return this.click("@occupationApplicant12Month")
+      },
+      mesAnteriorSolicitudTreball: () => {
+        return this.click("@recentSearchingOfWork")
       }
-      
   }]
 
 };
