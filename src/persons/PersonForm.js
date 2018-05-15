304ef9bf5dde60caf661955e77c4c726b5263aa2
Joan Serrano <john.voiden@gmail.com>
2018-05-15 09:48:40 +0200

    fix code duplication from master merge.

--- src/persons/PersonForm.js
+++ src/persons/PersonForm.js
@@ -182,6 +182,7 @@ let PersonForm = (props: Props) => {
                         <Trans>El règim penitenciari li permet treballar fora del centre</Trans>
                       </label>
                       }
+                      
                       <label>
                         <Field name="es_orfe_dels_dos_progenitors" checked={false} component={Checkbox}/>
                         <Trans>És orfe dels dos progenitors</Trans>
@@ -264,128 +265,7 @@ let PersonForm = (props: Props) => {
                       </label>
                       }
                     </Grid>
-                    }
-                    <label>
-                      <Trans>Total ingressos bruts 2016</Trans>
-                    </label>
-                    <Field name="ingressos_bruts" placeholder="0" type="number" fullWidth component={TextField}/>
-                    <label>
-                      <Trans>Grau discapacitat</Trans>
-                    </label>
-                    <Field name="grau_discapacitat" placeholder="0" type="number" component={TextField}/>
-                    {potTreballar &&
-                    <label>
-                      <Field name="victima_violencia_de_genere" checked={false} component={Checkbox}/>
-                      <Trans>Víctima violència de genere o domèstica</Trans>
-                    </label>
-                    }
-                    {esDona && victimaViolenciaDeGenere &&
-                    <label>
-                      <Field name="percep_prestacions_incompatibles_amb_la_feina" checked={false}
-                             component={Checkbox}/>
-                      <Trans>Perceb alguna ajuda que no li permeti treballar?</Trans>
-                    </label>
-                    }
-                    {esDona && !esFill &&
-                    <label>
-                      <Field name="es_divorciada_de_familia_reagrupada" checked={false} component={Checkbox}/>
-                      <Trans>És divorciada de familia reagrupada</Trans>
-                    </label>
-                    }
-                    {potTreballar && esDesocupat && inscritComADemandantDocupacio &&
-                    <label>
-                      <Field name="ingressat_en_centre_penitenciari" checked={false} component={Checkbox}/>
-                      <Trans>Ingressat en centre penitenciari</Trans>
-                    </label>
-                    }
-                    {ingressatEnCentrePenitenciari &&
-                    <label>
-                      <Field name="ingressat_en_centre_penitenciari_pot_treballar" checked={false}
-                             component={Checkbox}/>
-                      <Trans>El règim penitenciari li permet treballar fora del centre</Trans>
-                    </label>
-                    }
-                    <label>
-                      <Field name="es_orfe_dels_dos_progenitors" checked={false} component={Checkbox}/>
-                      <Trans>És orfe dels dos progenitors</Trans>
-                    </label>
-                    {potTreballar &&
-                    <label>
-                      <Field name="ha_treballat_a_l_estranger_6_mesos" checked={false} component={Checkbox}/>
-                      <Trans>Ha treballat a l&apos;estranger 6 mesos</Trans>
-                    </label>
-                    }
-                    {potTreballar && haTreballatALEstranger6Mesos &&
-                    <label>
-                      <Field name="ha_treballat_a_l_estranger_6_mesos_i_ha_retornat_en_els_ultims_12_mesos"
-                             checked={false} component={Checkbox}/>
-                      <Trans>Ha retornat d'aquest període de treball en els últims 12 mesos</Trans>
-                    </label>
-                    }
-                    {potTreballar && esDesocupat &&
-                    <label>
-                      <Field name="en_els_ultims_12_mesos_ha_fet_baixa_voluntaria_de_la_feina" checked={false}
-                             component={Checkbox}/>
-                      <Trans>En els últims 12 mesos ha fet baixa voluntaria de la feina</Trans>
-                    </label>
-                    }
-                    {potTreballar && esDesocupat &&
-                    <label>
-                      <Field name="ha_esgotat_prestacio_de_desocupacio" checked={false} component={Checkbox}/>
-                      <Trans>Ha esgotat una prestació contributiva o subsidi per desocupació</Trans>
-                    </label>
-                    }
-                    {potTreballar && esDesocupat &&
-                    <label>
-                      <Field name="inscrit_com_a_demandant_docupacio" checked={false} component={Checkbox}/>
-                      <Trans>És inscrit com a demandant d'ocupació</Trans>
-                    </label>
-                    }
-                    {potTreballar && esDesocupat && inscritComADemandantDocupacio &&
-                    <label>
-                      <Field name="demandant_d_ocupacio_durant_12_mesos" checked={false} component={Checkbox}/>
-                      <Trans>Ha estat inscrit de forma ininterrompuda com a demandant d'ocupació almenys 12
-                        mesos</Trans>
-                    </label>
-                    }
-                    {potTreballar && esDesocupat && inscritComADemandantDocupacio &&
-                    <label>
-                      <Field name="durant_el_mes_anterior_ha_presentat_solicituds_recerca_de_feina" checked={false}
-                             component={Checkbox}/>
-                      <Trans>Ha realitzat accions de recerca activa de feina en el mes anterior</Trans>
-                    </label>
-                    }
-                    {potTreballar && esDesocupat && inscritComADemandantDocupacio &&
-                    <label>
-                      <Field name="ha_estat_beneficiari_de_la_rai_en_els_ultims_12_mesos" checked={false}
-                             component={Checkbox}/>
-                      <Trans>Ha estat beneficiari de la RAI en els últims 12 mesos</Trans>
-                    </label>
-                    }
-                    {potTreballar && esDesocupat && inscritComADemandantDocupacio &&
-                    <label>
-                      <Field name="ha_estat_beneficiari_de_les_tres_rai_anteriors" checked={false}
-                             component={Checkbox}/>
-                      <Trans>Ha estat beneficiari de tres RAI anteriors</Trans>
-                    </label>
-                    }
-                    {esFill &&
-                    <label>
-                      <Field name="es_escolaritzat_entre_P3_i_4rt_ESO" component={Checkbox}/>
-                      <Trans>Escolatitzat entre P3 i 4rt d'ESO en una escola publica o concertada catalana?</Trans>
-                    </label>
-                    }
-                    {esFill &&
-                    <label>
-                      <Field name="en_acolliment" component={Checkbox}/> <Trans>En acolliment</Trans>
-                    </label>
-                    }
-                    {esFill &&
-                    <label>
-                      <Field name="beneficiari_fons_infancia" component={Checkbox}/>
-                      <Trans>Beneficiari/a fons infància</Trans>
-                    </label>
-                    }
+
                   </Grid>
                   <Grid item md={5} hidden={{smDown: true}}>
                     <DescriptionText/>
