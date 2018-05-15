9fccf4b32fed074050c36342741cb21947e04835
Joan Serrano <john.voiden@gmail.com>
2018-05-15 13:03:51 +0200

    Add Slashes after numbers(days/month) :)

--- src/components/Common/DateMask.js
+++ src/components/Common/DateMask.js
@@ -1,4 +1,4 @@
-const normalizeDate = value => {
+const normalizeDate = (value,previousValue) => {
   
   if (!value) {
     return value;
@@ -7,10 +7,20 @@ const normalizeDate = value => {
   const onlyNumbers = value.replace(/[^\d]/g, '');
 
   if (onlyNumbers.length <= 2) {
-    return onlyNumbers;
+
+      console.log(value.length +" "+ onlyNumbers.length);
+      console.log(value +" "+ onlyNumbers);
+      if(value.length === 2){
+        return `${onlyNumbers.slice(0, 2)}/`;
+      }
+      return onlyNumbers;
   }
   if (onlyNumbers.length <= 4) {
+    if(value.length === 5){
+       return `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2)}/`;
+    }  
     return `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2)}`;
+
   }
   if (onlyNumbers.length <= 8) {
     return `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2, 4)}/${onlyNumbers.slice(4, 12)}`;
