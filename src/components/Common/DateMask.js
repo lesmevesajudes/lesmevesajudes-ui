0a5523ec00f44fd344c87755b7bfdc2c8dbb6cbf
Joan Serrano <john.voiden@gmail.com>
2018-05-16 11:57:48 +0200

    Fixes in days, previousValues handlers.

--- src/components/Common/DateMask.js
+++ src/components/Common/DateMask.js
@@ -1,24 +1,35 @@
 const normalizeDate = (value,previousValue) => {
-  
   if (!value) {
     return value;
   } 
-
+  
   const actualYear = (new Date()).getFullYear();
   const onlyNumbers = value.replace(/[^\d]/g, '');
 
   if (onlyNumbers.length <= 2) {
+    if(value.length === 1 && value > 3){
+      return `${onlyNumbers.slice(0, 0)}`;
+    }
     if(value.length === 2){
+      if(previousValue.length === 3){
+        return `${onlyNumbers.slice(0, 1)}`;
+      }
+      if(value > 31){
+        return `${onlyNumbers.slice(0, 1)}`;
+      }
       return `${onlyNumbers.slice(0, 2)}/`;
     }
     return onlyNumbers;
   }
   if (onlyNumbers.length <= 4) {
     if(value.length === 5){
+        if(previousValue.length === 6){
+          return `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2)}`;
+        }
+            console.log("no te dejo borrar")
       return `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2)}/`;
     }  
     return `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2)}`;
-
   }
   if (onlyNumbers.length <= 8) {
     if(onlyNumbers.slice(4,5) > String(actualYear).charAt(0)){
@@ -40,7 +51,6 @@ const normalizeDate = (value,previousValue) => {
           return `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2, 4)}/${onlyNumbers.slice(4, 7)}`;
       }
     }
-
     return `${onlyNumbers.slice(0, 2)}/${onlyNumbers.slice(2, 4)}/${onlyNumbers.slice(4, 12)}`;
   }
 };
