85b0ffb12e86f76fac7c434bd593670392492587
Joan Serrano <john.voiden@gmail.com>
2018-05-31 12:35:01 +0200

    without shadow

--- src/components/ToggleButton/index.js
+++ src/components/ToggleButton/index.js
@@ -42,7 +42,7 @@ const styles = theme => ({
     backgroundColor:'rgb(23, 230, 109)'
   },
   test: {
-    boxShadow: "-1px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)"
+    //boxShadow: "-1px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)"
 
   }
 });
