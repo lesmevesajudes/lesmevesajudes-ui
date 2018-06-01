83abb121e8aa1f34d1c62b861eefb2644062706d
Joan Serrano <john.voiden@gmail.com>
2018-06-01 12:39:09 +0200

    clean warnings

--- src/components/Common/DescriptionText.js
+++ src/components/Common/DescriptionText.js
@@ -4,7 +4,6 @@ import Grid from '@material-ui/core/Grid';
 
 class DescriptionText extends Component {
   render() {
-    const {selectInput} = this.props
     return (
         <Grid item>
           Input seleccionat: {this.props.selectInput}
--- src/persons/PersonForm.js
+++ src/persons/PersonForm.js
@@ -49,7 +49,6 @@ type Props = {
 
 let PersonForm = (props: Props) => {
   const {
-    active,
     cobraAlgunTipusDePensioNoContributiva,
     edat,
     esDesocupat,
