bf5e9c8b816a2d764949518aa53350d869dff234
Joan Serrano <john.voiden@gmail.com>
2018-06-01 12:19:04 +0200

    DescriptionText upgrade

--- src/components/Common/DescriptionText.js
+++ src/components/Common/DescriptionText.js
@@ -1,24 +1,20 @@
 import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid';
+import {connect} from 'react-redux';
 
 class DescriptionText extends Component {
   render() {
+
     return (
         <Grid item>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          quis gravida erat. Donec ac nulla et ligula facilisis auctor sed
-          eu enim. In hac habitasse platea dictumst. In ultrices rhoncus
-          felis faucibus blandit. Curabitur scelerisque, urna nec finibus
-          molestie, orci est imperdiet arcu, et fermentum velit est nec
-          nisi. Morbi mollis vel purus sed fringilla. Aliquam ut tellus at
-          justo varius vulputate non in justo. Ut at lorem at augue
-          facilisis varius. Aliquam semper quam a dolor porttitor, et
-          condimentum velit malesuada. In ac diam quis dolor lacinia
-          varius ut sit amet ligula. Nulla facilisi. Integer vestibulum
-          neque ac sagittis consequat. Morbi non odio lorem.
+          Input seleccionat: {this.props.active}
         </Grid>
     );
   }
 }
-
-export default DescriptionText;
+function mapStateToProps(state){
+  return{
+        active: state.form.PersonForm.active
+  };
+}
+export default connect(mapStateToProps)(DescriptionText);
