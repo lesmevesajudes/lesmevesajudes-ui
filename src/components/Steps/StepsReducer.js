bfb2f6fd25c27dba3288605212794959845481a4
Joan Serrano <john.voiden@gmail.com>
2018-06-28 15:17:33 +0200

    fix warning(?)

--- src/components/Steps/StepsReducer.js
+++ src/components/Steps/StepsReducer.js
@@ -34,7 +34,7 @@ export default (state: StepsState = initial, action: StepAction): StepsState => 
           ...state,
           current_step: action.index,
           max_step_reached: action.index
-        }
+        };
       };
     case 'BUTTONS_VISIBLE':
       return {
