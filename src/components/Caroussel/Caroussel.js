c1885141ce753308202a59132631acf6c21cdf0e
jvalduvieco <jvalduvieco@gmail.com>
2018-07-03 21:21:51 +0200

    remove /es/

--- src/components/Caroussel/Caroussel.js
+++ src/components/Caroussel/Caroussel.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {ButtonBack, ButtonNext, CarouselProvider, Slide, Slider} from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-import {Link} from "react-router-dom";
-import Trans from "react-i18next/dist/es/Trans";
-import Typography from "@material-ui/core/es/Typography/Typography";
-import {Button, Grid} from "@material-ui/core/";
+import {Link} from 'react-router-dom';
+import {Trans} from 'react-i18next';
+import Typography from '@material-ui/core/Typography';
+import {Button, Grid} from '@material-ui/core/';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
@@ -91,20 +91,20 @@ export default class extends React.Component {
           <Slider>
             {content.map((slideContent, index) =>
                 <Slide key={index} index={index}>
-                  <Grid container direction='column' className="sliderItem">
+                  <Grid container direction='column' className='sliderItem'>
                     <Grid item justify='flex-start' style={{height: 90}}>
-                      <Typography variant='title' className="sliderTitle" gutterBottom>
+                      <Typography variant='title' className='sliderTitle' gutterBottom>
                         {slideContent.title}
                       </Typography>
                     </Grid>
                     <Grid item justify='flex-start' style={{height: 120}}>
-                      <Typography className="sliderBody">
+                      <Typography className='sliderBody'>
                         {slideContent.body}
                       </Typography>
                     </Grid>
                     <Grid item xs={12} sm={12}>
                       <Link to={slideContent.link}>
-                      <Button color="primary" variant="contained" className="sliderButton"> Més informació</Button>
+                        <Button color='primary' variant='contained' className='sliderButton'> Més informació</Button>
                       </Link>
                     </Grid>
                   </Grid>
