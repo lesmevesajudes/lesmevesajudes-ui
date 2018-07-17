811906e444675e655628a028b84e1b829831571d
jvalduvieco <jvalduvieco@gmail.com>
2018-07-17 06:46:21 +0200

    tune questions to enable benefit for losing home due mortage execution

--- src/residence/ResidenceForm.js
+++ src/residence/ResidenceForm.js
@@ -40,7 +40,9 @@ type Props = {
   haParticipatEnUnProcesDeMediacio: boolean,
   haSeleccionatAlgunaRelacioAmbLHabitatge: boolean,
   initialValues: ?ResidenceData,
+  noTeHabitatgeFix: boolean,
   personesQuePodenTenirContracte: Map<PersonID, Person>,
+  properContracteDeLloguer: boolean,
   state: any,
   teAlgunaPropietat: boolean,
   teHabitatgeHabitual: boolean,
@@ -59,11 +61,14 @@ const ResidenceForm = (props: Props) => {
     haEstatDesnonat,
     haParticipatEnUnProcesDeMediacio,
     haSeleccionatAlgunaRelacioAmbLHabitatge,
+    noTeHabitatgeFix,
+    properContracteDeLloguer,
     teAlgunaPropietat,
     teHabitatgeHabitual,
     titularContracteLloguer,
     titularContracteHipoteca
   } = props;
+  console.log("haEstatDesnonat: ", haEstatDesnonat, noTeHabitatgeFix, (teHabitatgeHabitual || haEstatDesnonat));
   return (
       <Grid container className='bg-container'>
         <Grid item xs={12} sm={12} className='titleContainer'>
@@ -94,22 +99,6 @@ const ResidenceForm = (props: Props) => {
                     <MenuItem value='altres'><Trans>Altres</Trans></MenuItem>
                   </MultipleAnswerQuestion>
 
-                  {teHabitatgeHabitual &&
-                  <Question required name='codi_postal_habitatge' placeholder='08000' normalize={onlyNumbers}
-                            component={TextField} validate={seemsPostalCode}>
-                    <Trans>Codi postal on es troba l'habitatge</Trans>
-                  </Question>}
-
-                  {esLlogater &&
-                  <MoneyQuestion name='import_del_lloguer'>
-                    <Trans>Quin és l’import mensual d’aquest lloguer?</Trans>
-                  </MoneyQuestion>}
-
-                  {existeixHipoteca &&
-                  <MoneyQuestion name='import_de_la_hipoteca'>
-                    <Trans>Quina és la quota mensual de la seva hipoteca?</Trans>
-                  </MoneyQuestion>}
-
                   {esLlogater &&
                   <YesNoQuestion name='existeix_deute_en_el_pagament_del_lloguer'>
                     <Trans>Existeix un deute en el pagament del lloguer?</Trans>
@@ -136,7 +125,31 @@ const ResidenceForm = (props: Props) => {
                     </Trans>
                   </YesNoQuestion>}
 
-                  {esLlogater && (existeixDeutePagamentLloguer || haParticipatEnUnProcesDeMediacio || haEstatDesnonat) &&
+                  {noTeHabitatgeFix && haEstatDesnonat &&
+                  <YesNoQuestion name='proper_contracte_de_lloguer'>
+                    <Trans>
+                      Ha signat un contracte de lloguer per al seu nou habitatge o està en procés de fer-ho en els
+                      propers 60 díes?
+                    </Trans>
+                  </YesNoQuestion>}
+
+                  {(teHabitatgeHabitual || properContracteDeLloguer) &&
+                  <Question required name='codi_postal_habitatge' placeholder='08000' normalize={onlyNumbers}
+                            component={TextField} validate={seemsPostalCode}>
+                    <Trans>Codi postal on es troba l'habitatge</Trans>
+                  </Question>}
+
+                  {(esLlogater || properContracteDeLloguer) &&
+                  <MoneyQuestion name='import_del_lloguer'>
+                    <Trans>Quin és l’import mensual d’aquest lloguer?</Trans>
+                  </MoneyQuestion>}
+
+                  {existeixHipoteca &&
+                  <MoneyQuestion name='import_de_la_hipoteca'>
+                    <Trans>Quina és la quota mensual de la seva hipoteca?</Trans>
+                  </MoneyQuestion>}
+
+                  {(esLlogater || properContracteDeLloguer) && (existeixDeutePagamentLloguer || haParticipatEnUnProcesDeMediacio || haEstatDesnonat) &&
                   <MultipleAnswerQuestion name='titular_contracte_de_lloguer_id'
                                           label={<Trans>Persona titular del contracte de lloguer</Trans>}>
                     {props.personesQuePodenTenirContracte.valueSeq().map((persona) => (
@@ -217,7 +230,7 @@ const ResidenceForm = (props: Props) => {
                     <Trans>Fa més de 12 mesos que no paga les quotes de la hipoteca?</Trans>
                   </YesNoQuestion>}
 
-                  {teHabitatgeHabitual &&
+                  {(teHabitatgeHabitual || properContracteDeLloguer) &&
                   <YesNoQuestion name='tinc_alguna_propietat_a_part_habitatge_habitual'>
                     <Trans>
                       Alguna persona que conviu amb vostè té alguna propietat a part de l'habitatge habitual
@@ -229,7 +242,7 @@ const ResidenceForm = (props: Props) => {
                     <Trans>Disposa de l’usdefruit d’aquesta propietat?</Trans>
                   </YesNoQuestion>}
 
-                  {esLlogater && existeixDeutePagamentLloguer &&
+                  {((esLlogater && existeixDeutePagamentLloguer) || properContracteDeLloguer) &&
                   <YesNoQuestion name='es_ocupant_dun_habitatge_gestionat_per_lagencia_de_lhabitatge'>
                     <Trans>
                       És ocupant d’un habitatge gestionat per l’Agència de l’Habitatge de Catalunya o de l’Institut
@@ -267,6 +280,8 @@ function mapStateToProps(state) {
   const existeixHipoteca = selector(state, 'relacio_habitatge') === 'propietari_hipoteca';
   const esCessio = selector(state, 'relacio_habitatge') === 'cessio';
   const haSeleccionatAlgunaRelacioAmbLHabitatge = selector(state, 'relacio_habitatge') != null;
+  const noTeHabitatgeFix = selector(state, 'relacio_habitatge') === 'no_en_te';
+  const properContracteDeLloguer = selector(state, 'proper_contracte_de_lloguer');
   const titularContracteLloguer = state.persons.get(selector(state, 'titular_contracte_de_lloguer_id'));
   const titularContracteHipoteca = state.persons.get(selector(state, 'titular_hipoteca_id'));
 
@@ -281,7 +296,9 @@ function mapStateToProps(state) {
     haParticipatEnUnProcesDeMediacio: selector(state, 'ha_participat_en_un_proces_de_mediacio'),
     haSeleccionatAlgunaRelacioAmbLHabitatge: haSeleccionatAlgunaRelacioAmbLHabitatge,
     initialValues: state.residence,
+    noTeHabitatgeFix: noTeHabitatgeFix,
     personesQuePodenTenirContracte: state.persons.filter((persona) => !esFill(persona)),
+    properContracteDeLloguer: properContracteDeLloguer,
     teAlgunaPropietat: selector(state, 'tinc_alguna_propietat_a_part_habitatge_habitual'),
     teHabitatgeHabitual: esLlogater || esPropietari || esCessio,
     titularContracteLloguer: titularContracteLloguer,
