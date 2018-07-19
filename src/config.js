69e510e1eb5e1b224468e0711b60dbf0ef1c0415
jvalduvieco <jvalduvieco@gmail.com>
2018-07-19 19:55:10 +0200

    use '

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:2000";
-const ENVIRONMENT = process.env.NODE_ENV || "development";
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:2000';
+const ENVIRONMENT = process.env.NODE_ENV || 'development';
 
 export {API_URL, ENVIRONMENT};
