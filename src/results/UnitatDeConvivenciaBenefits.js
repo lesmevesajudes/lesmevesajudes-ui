e5bf43f6cea3b226fe0c4d6175f838f7a93209cd
Joan Valduvieco <jvalduvieco@gmail.com>
2018-07-25 12:45:49 +0200

    fix text

--- src/results/UnitatDeConvivenciaBenefits.js
+++ src/results/UnitatDeConvivenciaBenefits.js
@@ -8,9 +8,8 @@ const possibleBenefits = [
   {
     ID: 'HA_001',
     name: <Trans>Prestacions econòmiques d'urgència social derivades de la mediació a Barcelona</Trans>,
-    periode: 'mes',
     url: '/ajuts/lloguer_mediacio',
-    amountText: 'Entre 20 - 300 € /mes durant un màxim de 12 mesos',
+    amountText: 'Fins a 300 € /mes màxim 12 mesos',
     from: newDate(2018, 6, 12),
     to: newDate(2018, 12, 7)
   },
@@ -18,7 +17,7 @@ const possibleBenefits = [
     ID: 'HA_002',
     name: <Trans>Prestacions econòmiques d’especial urgència davant la pèrdua de l’habitatge per desnonament o execució
       hipotecària</Trans>,
-    periode: 'mes durant un màxim de 12 mesos',
+    amountText: 'Fins a 200 € /mes màxim 12 mesos',
     url: '/ajuts/desnonament_especial_urgencia',
     from: undefined,
     to: undefined
@@ -27,7 +26,7 @@ const possibleBenefits = [
     ID: 'HA_003',
     name: <Trans>Prestacions econòmiques d’especial urgència per al pagament de quotes d'amortització
       hipotecària</Trans>,
-    periode: 'pagament únic anual',
+    amountText: 'Fins a 3000 € /any pagament únic',
     url: '/ajuts/hipoteca_especial_urgencia',
     from: undefined,
     to: undefined
@@ -35,7 +34,7 @@ const possibleBenefits = [
   {
     ID: 'HA_004',
     name: <Trans>Prestacions econòmiques d’especial urgència per al pagament de deutes del lloguer</Trans>,
-    periode: 'pagament únic anual',
+    amountText: 'Fins a 3000 € /any pagament únic',
     url: '/ajuts/lloguer_especial_urgencia',
     from: undefined,
     to: undefined
@@ -43,7 +42,7 @@ const possibleBenefits = [
   {
     ID: 'HA_005',
     name: <Trans>Subvencions de pagament de lloguer (MIFO)</Trans>,
-    periode: 'mes durant un màxim de 12 mesos',
+    amountText: 'Fins a 200 € /mes màxim 12 mesos',
     url: '/ajuts/lloguer',
     from: newDate(2018, 5, 19),
     to: newDate(2018, 6, 29)
