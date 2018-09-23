f6236a8c7dac270a072e81b6d084dacab64868ca
Joan Valduvieco <jvalduvieco@gmail.com>
2018-09-23 23:26:48 +0200

    check if undefined to decide if a field is filled or not.

--- src/shared/formValidators.js
+++ src/shared/formValidators.js
@@ -5,7 +5,7 @@ export const anysEmpadronatInferiorAEdat = (value, allValues) =>
   value && value > parseInt(allValues.edat, 10)
     ? <Trans>Els anys d'empadronament han de ser iguals o inferiors a l'edat</Trans>
     : undefined;
-export const required = value => value ? undefined : <Trans>Aquest camp és requerit</Trans>;
+export const required = value => typeof value !== 'undefined' ? undefined : <Trans>Aquest camp és requerit</Trans>;
 export const pncInclosAIngressosBruts = (value, allValues) =>
   value && value > parseInt(allValues.ingressos_bruts, 10)
     ? <Trans>Els ingressos per pensions no contributives has d'estar inclosos en els ingressos bruts</Trans>
