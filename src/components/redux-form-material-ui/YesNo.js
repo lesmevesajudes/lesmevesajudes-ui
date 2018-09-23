651546b63c6bd57d8bf892969ae962140962aa6a
Joan Valduvieco <jvalduvieco@gmail.com>
2018-09-23 22:59:37 +0200

    Make YesNo show errors. Hackish solution need to find how to use material-ui styles.

--- src/components/redux-form-material-ui/YesNo.js
+++ src/components/redux-form-material-ui/YesNo.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import MultipleChoice from '../MultipleChoice';
-import {Choice} from '../Choice';
-import {Grid} from '@material-ui/core';
+import React from "react";
+import MultipleChoice from "../MultipleChoice";
+import { Choice } from "../Choice";
+import { Grid } from "@material-ui/core";
 
-const YesNo = ({input}) => (
+const YesNo = ({input, meta}) => {
+
+
+  return <Grid container direction='column'>
     <Grid container direction='row'>
       <MultipleChoice
-          currentState={input.value}
-          onFocus={input.onFocus}
-          optionSelected={input.onChange}
+        currentState={input.value}
+        onFocus={input.onFocus}
+        optionSelected={input.onChange}
+        meta
       >
         <Choice value={true} variant='outlined'>
           Si
@@ -18,5 +22,18 @@ const YesNo = ({input}) => (
         </Choice>
       </MultipleChoice>
     </Grid>
-);
+    <Grid item>
+      {meta.touched && meta.error &&
+      <span style={{
+        color: '#f44336',
+        margin: 0,
+        fontSize: "0.8571428571428571rem",
+        textAlign: "left",
+        marginTop: "8px",
+        minHeight: "1em",
+        lineHeight: "1em"
+      }}>{meta.error}</span>}
+    </Grid>
+  </Grid>;
+};
 export default YesNo;
