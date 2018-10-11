3fcde2273057bb131fc5c2e4c703afd2d0686d77
jvalduvieco <jvalduvieco@gmail.com>
2018-10-11 21:45:59 +0200

    Remove unfinished test

--- src/family/detectaFamilies.test.js
+++ src/family/detectaFamilies.test.js
@@ -49,31 +49,6 @@ describe('Given a set of custodies detect families', () => {
         );
       });
 
-  it('Detects one family from multiple custodies', () => {
-    expect(
-        detectaFamiliesAPartirDeCustodies(
-            {
-              '12a030f5-31f1-43d0-828b-168eb55471db': {
-                primer: '7307eb57-41ef-40f5-861a-1dcc72ac3f1e',
-                segon: '2844f3a8-a334-4baf-8f4a-762d3b7b1543'
-              },
-              'fc9c8238-7992-4475-8680-e56942ca7c3d': {
-                primer: '7307eb57-41ef-40f5-861a-1dcc72ac3f1e',
-                segon: 'no-conviu'
-              }
-            }, Map({}))
-    ).toEqual(
-        {
-          '2844f3a8-a334-4baf-8f4a-762d3b7b15437307eb57-41ef-40f5-861a-1dcc72ac3f1e': {
-            'menors': ['12a030f5-31f1-43d0-828b-168eb55471db', 'fc9c8238-7992-4475-8680-e56942ca7c3d'],
-            'sustentadors_i_custodia': ['2844f3a8-a334-4baf-8f4a-762d3b7b1543', '7307eb57-41ef-40f5-861a-1dcc72ac3f1e'],
-            'monoparental': false,
-            'tipus_custodia': 'total'
-          }
-        }
-    );
-  });
-
       it('Detects two families from multiple custodies', () => {
         expect(
             detectaFamiliesAPartirDeCustodies(
