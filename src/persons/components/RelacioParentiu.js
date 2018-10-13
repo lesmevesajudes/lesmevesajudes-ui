a8522922053b6ce5c7e195062a93e798770e8218
jvalduvieco <jvalduvieco@gmail.com>
2018-10-13 19:19:16 +0200

    Remove test props.. need to think how to do proper testing

--- src/persons/components/RelacioParentiu.js
+++ src/persons/components/RelacioParentiu.js
@@ -1,51 +1,51 @@
-import React from 'react';
-import MenuItem from '@material-ui/core/MenuItem';
-import {Trans} from 'react-i18next';
 import Grid from '@material-ui/core/Grid';
-import MultipleAnswerQuestion from './MultipleAnswerQuestion';
 import Icon from '@material-ui/core/Icon';
-import { required } from "../../shared/formValidators";
+import MenuItem from '@material-ui/core/MenuItem';
+import React from 'react';
+import {Trans} from 'react-i18next';
+import {required} from "../../shared/formValidators";
+import MultipleAnswerQuestion from './MultipleAnswerQuestion';
 
 export const RelacioParentiu = () =>
     <Grid item>
       <MultipleAnswerQuestion name='relacio_parentiu' label={<Trans>Aquesta persona és el/la seu/va?</Trans>} validate={[required]}>
-        <MenuItem data-test='parella' value='parella'>
+        <MenuItem value='parella'>
           <Trans>Cònjuge / parella</Trans>
         </MenuItem>
-        <MenuItem data-test='fill' value='fill'>
+        <MenuItem value='fill'>
           <Trans>Fill/a</Trans>
         </MenuItem>
-        <MenuItem data-test='fillastre' value='fillastre'>
+        <MenuItem value='fillastre'>
           <Trans>Fillastre/a (o fill/a de la parella actual)</Trans>
         </MenuItem>
-        <MenuItem data-test='net' value='net'>
+        <MenuItem value='net'>
           <Trans>Nét/a</Trans>
         </MenuItem>
-        <MenuItem data-test='infant_acollit' value='infant_acollit'>
+        <MenuItem value='infant_acollit'>
           <Trans>Infant en acolliment </Trans><Icon>info</Icon>
         </MenuItem>
-        <MenuItem data-test='pare' value='pare'>
+        <MenuItem value='pare'>
           <Trans>Pare o mare</Trans>
         </MenuItem>
-        <MenuItem data-test='avi' value='avi'>
+        <MenuItem value='avi'>
           <Trans>Avi / Àvia</Trans>
         </MenuItem>
-        <MenuItem data-test='sogre' value='sogre'>
+        <MenuItem value='sogre'>
           <Trans>Sogre/a</Trans>
         </MenuItem>
-        <MenuItem data-test='germa' value='germa'>
+        <MenuItem value='germa'>
           <Trans>Germà/germana</Trans>
         </MenuItem>
-        <MenuItem data-test='cunyat' value='cunyat'>
+        <MenuItem value='cunyat'>
           <Trans>Cunyat/da</Trans>
         </MenuItem>
-        <MenuItem data-test='gendre' value='gendre'>
+        <MenuItem value='gendre'>
           <Trans>Gendre/Nora/Parella del meu fill/a</Trans>
         </MenuItem>
-        <MenuItem data-test='altres' value='altres'>
+        <MenuItem value='altres'>
           <Trans>Altres familiars</Trans>
         </MenuItem>
-        <MenuItem data-test='cap' value='cap'>
+        <MenuItem value='cap'>
           <Trans>Sense relació de parentiu</Trans>
         </MenuItem>
       </MultipleAnswerQuestion>
--- src/persons/components/SituacioLaboral.js
+++ src/persons/components/SituacioLaboral.js
@@ -1,35 +1,33 @@
-import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
+import React from 'react';
 import {Trans} from 'react-i18next';
+import {required} from "../../shared/formValidators";
 import MultipleAnswerQuestion from './MultipleAnswerQuestion';
-import { required } from "../../shared/formValidators";
 
 export const SituacioLaboral = () =>
     <MultipleAnswerQuestion label={<Trans>Indiqui la seva situació laboral:</Trans>} name='situacio_laboral' validate={[required]}>
-      <MenuItem data-test='treball_compte_propi' value='treball_compte_propi'>
+      <MenuItem value='treball_compte_propi'>
         <Trans>Treballa per compte propi</Trans>
       </MenuItem>
-      <MenuItem data-test='treball_compte_daltri_jornada_complerta'
-                value='treball_compte_daltri_jornada_complerta'>
+      <MenuItem value='treball_compte_daltri_jornada_complerta'>
         <Trans>Treballa per compte d'altri jornada complerta</Trans>
       </MenuItem>
-      <MenuItem data-test='treball_compte_daltri_jornada_parcial'
-                value='treball_compte_daltri_jornada_parcial'>
+      <MenuItem value='treball_compte_daltri_jornada_parcial'>
         <Trans>Treballa per compte d'altri jornada parcial</Trans>
       </MenuItem>
-      <MenuItem data-test='aturat' value='aturat'>
+      <MenuItem value='aturat'>
         <Trans>Aturat/ada</Trans>
       </MenuItem>
-      <MenuItem data-test='tasques_de_la_llar' value='tasques_de_la_llar'>
+      <MenuItem value='tasques_de_la_llar'>
         <Trans>Tasques de la llar</Trans>
       </MenuItem>
-      <MenuItem data-test='estudiant' value='estudiant'>
+      <MenuItem value='estudiant'>
         <Trans>Estudiant o pràctiques sense remunerar</Trans>
       </MenuItem>
-      <MenuItem data-test='jubilat' value='jubilat'>
+      <MenuItem value='jubilat'>
         <Trans>Jubilat/ada o prejubilat/ada</Trans>
       </MenuItem>
-      <MenuItem data-test='altres' value='altres'>
+      <MenuItem value='altres'>
         <Trans>Altres situacions</Trans>
       </MenuItem>
     </MultipleAnswerQuestion>;
