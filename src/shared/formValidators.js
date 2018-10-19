99ea4ce72dfaf3f8a21828bacbc4a662a78dfa6c
Joan Valduvieco <jvalduvieco@gmail.com>
2018-10-19 12:51:21 +0200

    A null string is not a valid content for a required field

--- src/shared/formValidators.js
+++ src/shared/formValidators.js
@@ -5,7 +5,7 @@ export const anysEmpadronatInferiorAEdat = (value, allValues) =>
   value && value > parseInt(allValues.edat, 10)
     ? <Trans>Els anys d'empadronament han de ser iguals o inferiors a l'edat</Trans>
     : undefined;
-export const required = value => typeof value !== 'undefined' ? undefined : <Trans>Aquest camp és requerit</Trans>;
+export const required = value => typeof value !== 'undefined' && value !== "" ? undefined : <Trans>Aquest camp és requerit</Trans>;
 export const email = value =>
     value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
         <Trans>Correu electrònic invàlid</Trans> : undefined;
