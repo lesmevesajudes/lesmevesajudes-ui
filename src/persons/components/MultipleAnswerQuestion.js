6c1eb145150d6f133e702429ed2417a2a5d1f761
jvalduvieco <jvalduvieco@gmail.com>
2018-10-21 19:26:30 +0200

    Make select show validation errors

--- src/persons/components/MultipleAnswerQuestion.js
+++ src/persons/components/MultipleAnswerQuestion.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import {Select} from 'redux-form-material-ui';
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import {TextField} from 'redux-form-material-ui';
 import {IRemoveMyValueWhenUnmountedField} from "../../components/IRemoveMyValueWhenUnmountedField";
 
 type Props = {
   label: Object,
 }
 const MultipleAnswerQuestion = (props: Props) =>
-    <Grid item>
+    <Grid item xs={12}>
       <label>
         <Typography>
           {props.label}
         </Typography>
       </label>
-      <IRemoveMyValueWhenUnmountedField {...props} component={Select} fullWidth>
+      {/* We're breaking a bit material guides here as select should show label when there is no value selected */}
+      <IRemoveMyValueWhenUnmountedField {...props} component={TextField} select fullWidth label=''>
         {props.children}
       </IRemoveMyValueWhenUnmountedField>
     </Grid>;
