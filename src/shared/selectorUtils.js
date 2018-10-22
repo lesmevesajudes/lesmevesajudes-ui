2587d682de1e15e87c4e3d230c7fc5d2faf2d52f
Joan Valduvieco <jvalduvieco@gmail.com>
2018-10-22 12:25:28 +0200

    A person that is 16 years or older can hold custody of another.

--- src/shared/selectorUtils.js
+++ src/shared/selectorUtils.js
@@ -1,7 +1,7 @@
 import {Person} from '../persons/PersonTypes';
 
 export const esFill = (persona: Person) => persona.edat <= 16 || persona.es_escolaritzat_entre_P3_i_4rt_ESO;
-export const esSustentador = (persona: Person) => persona.edat > 16
+export const esSustentador = (persona: Person) => persona.edat >= 16
     && (persona.is_the_person_in_front_of_the_computer ||
         (typeof persona.relacio_parentiu === 'string' && persona.relacio_parentiu !== 'cap'));
 export const esInfantAcollit = (persona: Person) => persona.rol === 'infant_acollit';
