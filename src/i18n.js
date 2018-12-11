681992067505dd3054e865f121f99f4273ae735d
jvalduvieco <jvalduvieco@gmail.com>
2018-12-11 13:16:47 +0100

    i18next vs drupal happiness

--- src/i18n.js
+++ src/i18n.js
@@ -1,22 +1,33 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import LngDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-xhr-backend';
 import {reactI18nextModule} from 'react-i18next';
+import pathTransformLngDetector from './shared/pathTransformLngDetector';
 
 const {homepage} = require('../package.json');
+const lngDetector = new LngDetector();
+lngDetector.addDetector(pathTransformLngDetector);
+
+const languageDetectorOptions = {
+  order: ['pathTransform', 'querystring', 'navigator'],
+  lookupFromPathIndex: 0,
+  transforms: {'ca': 'ca-ES'},
+};
 
 i18n
   .use(Backend)
-  .use(LanguageDetector)
+    .use(lngDetector)
   .use(reactI18nextModule)
   .init({
     fallbackLng: 'ca-ES',
+
+    detection: languageDetectorOptions,
 
     // have a common namespace used around the full app
     ns: ['translations', 'index'],
     defaultNS: 'translations',
 
-    debug: true,
+    debug: false,
 
     interpolation: {
       escapeValue: false // not needed for react!!
---
+++ src/shared/pathTransformLngDetector.js
@@ -0,0 +1,24 @@
+export default {
+  name: 'pathTransform',
+
+  lookup(options) {
+    let found;
+    if (typeof window !== 'undefined') {
+      const language = window.location.pathname.match(/\/([a-zA-Z-]*)/g);
+      if (language instanceof Array) {
+        if (typeof options.lookupFromPathIndex === 'number') {
+          if (typeof language[options.lookupFromPathIndex] !== 'string') {
+            return undefined;
+          }
+          found = language[options.lookupFromPathIndex].replace('/', '');
+        } else {
+          found = language[0].replace('/', '');
+        }
+      }
+    }
+    if (typeof options.transforms[found] !== 'undefined') {
+      found = options.transforms[found]
+    }
+    return found;
+  }
+};
