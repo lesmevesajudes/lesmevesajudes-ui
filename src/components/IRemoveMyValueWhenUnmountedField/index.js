d6b5744c58370ad73e829253aaea8753f5554073
jvalduvieco <jvalduvieco@gmail.com>
2018-12-18 20:12:52 +0100

    Remove value from unregistered fields

--- src/components/IRemoveMyValueWhenUnmountedField/index.js
+++ src/components/IRemoveMyValueWhenUnmountedField/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {clearFields, Field} from 'redux-form';
+import {change, clearFields, Field} from 'redux-form';
 
 export class IRemoveMyValueWhenUnmountedField extends Field {
   componentWillUnmount() {
     super.componentWillUnmount();
     const {name} = this.props;
     let action;
-
+    this.context._reduxForm.dispatch(change(this.context._reduxForm.form, name, null));
     action = clearFields(this.context._reduxForm.form, false, false, name);
 
     this.context._reduxForm.dispatch(action);
