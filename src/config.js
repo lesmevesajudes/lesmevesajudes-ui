48efea358bf749e59d00a20c7ce80bf59c773df2
jvalduvieco <jvalduvieco@gmail.com>
2019-12-23 12:04:02 +0100

    Use public en Url to fetch translations

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ const SHOW_REPORT_BUG = process.env.REACT_APP_SHOW_REPORT_BUG === 'true';
 const REPORT_BUG_URL = process.env.REACT_APP_REPORT_BUG_URL || 'http://localhost:3000/api/simulation_reports';
 const SIMULATION_STORE_URL = process.env.REACT_APP_SIMULATION_STORE_URL || 'http://localhost:3000/api/simulations';
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
-const STATIC_ROOT = process.env.PUBLIC_URL || '';
+const STATIC_ROOT = process.env.REACT_APP_PUBLIC_URL || '';
 export {API_URL, SHOW_REPORT_BUG, REPORT_BUG_URL, ENVIRONMENT, SIMULATION_STORE_URL, STATIC_ROOT};
