7e1a61167ff819b471af1a769dd49943ac0f4fb4
Aritz <aritz>
2020-10-23 16:32:59 +0200

    get text from i18n + more aids table columns and  pagination + laboral count ranking

--- src/dashboard/DashboardComponent.js
+++ src/dashboard/DashboardComponent.js
@@ -27,6 +27,7 @@ type Props = {
   ageData: Object,
   housingData: Object,
   dispatch: Function,
+  aids: List,
   retrieveDashboard: any
 };
 
@@ -38,110 +39,8 @@ var laboralData={};
 var schoolData={};
 var disabledData={};
 var violenceData={};
-var housingData = {} // simulation - residence - relacio_habitatge
-
-// return format: [["GG_270_mensual",3],["GE_051_03_mensual",2],...]
-
-
-const aidsData = [{
-	"codi": "HA_077_01",
-	"descripcio": "Prestacions econòmiques d'urgència social derivades de la mediació a Barcelona",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":"Habitatge"
-},
-{
-	"codi": "HG_077_02",
-	"descripcio": "Prestacions econòmiques d’especial urgència davant la pèrdua de l’habitatge per desnonament o execució hipotecària",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":"Habitatge"
-},{
-	"codi": "HG_077_03",
-	"descripcio": "Prestacions econòmiques d’especial urgència per al pagament de quotes d'amortització hipotecària",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":"Habitatge"
-},{
-	"codi": "HG_077_04",
-	"descripcio": "Prestacions econòmiques d’especial urgència per al pagament de deutes del lloguer",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":"Habitatge"
-},{
-	"codi": "HA_077_04_01",
-	"descripcio": "Ajut complementari a les prestacions econòmiques d’especial urgència per al pagament de deutes del lloguer",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":"Habitatge"
-},{
-	"codi": "HE_077_00",
-	"descripcio": "Subvencions per al pagament de lloguer",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":"Habitatge"
-},{
-	"codi": "EG_233",
-	"descripcio": "Beques menjador escolar de Barcelona",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":"Educació"
-},{
-	"codi": "GA_234_01",
-	"descripcio": "Vincles - Gratuïta",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-},{
-	"codi": "GA_234_02",
-	"descripcio": "Vincles - Amb tauleta pròpia",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-},{
-	"codi": "GG_270",
-	"descripcio": "Renda garantida ciutadana",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-},{
-	"codi": "GE_051_04",
-	"descripcio": "Renda activa d'inserció aturats de llarga durada",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-},{
-	"codi": "GE_051_01",
-	"descripcio": "Renda activa d'inserció discapacitat 33%",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-},{
-	"codi": "GE_051_02",
-	"descripcio": "Renda activa d'inserció per a emigrants retornats",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-},{
-	"codi": "GE_051_03",
-	"descripcio": "Renda activa d'inserció per a víctimes de violència de gènere o domèstica",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-},{
-	"codi": "GA_246_01",
-	"descripcio": " Targeta rosa - Gratuïta",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-},{
-	"codi": "GA_246_02",
-	"descripcio": "Targeta rosa - Reduïda",
-	"data_inici": null,
-	"data_fi": null,
-	"tipus":null
-}]
-
+var housingData = {}
+var aidsData = []
 
 export const DashboardPage = (props :Props) => {
 
@@ -157,6 +56,7 @@ export const DashboardPage = (props :Props) => {
     ageData = props.ageData;
     housingData = props.housingData;
     positiveNegativeData = props.positiveNegativeData;
+    aidsData = props.aids;
   }
 
   return (
@@ -206,6 +106,7 @@ function mapStateToProps(state) {
     ageData: state.dashboard.ageData,
     housingData: state.dashboard.housingData,
     positiveNegativeData: state.dashboard.positiveNegativeData,
+    aids: state.dashboard.aids,
   };
   return props;
 }
