fcea4b67f9b49893405d25de3ad62756478a19dc
Aritz <aritz>
2020-11-25 22:08:15 +0100

    download aids table pdf

--- src/dashboard/aids/AidsTable.js
+++ src/dashboard/aids/AidsTable.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination } from '@material-ui/core';
+import { Paper, Table, TableBody, TableFooter, TableCell, TableContainer, TableHead, TableRow, TablePagination } from '@material-ui/core';
 import {useTranslation} from 'react-i18next';
 import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
 import CancelRounded from '@material-ui/icons/CancelRounded';
 import {FilterType} from './AidsDashboardTypes';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import Button from '@material-ui/core/Button';
+import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
+
 
 type Props = {
   aids: List ,
@@ -30,6 +35,31 @@ const AidsTable = (props: Props) => {
   const getAidTimeStamp = aid => parseInt(aid.data_inici.substring(0,4)) + parseInt(aid.data_inici.substring(5,7))
   const getFilterTimeStamp = date => date.getFullYear() + date.getMonth() + 1
 
+  var allAids = []
+  var paginatedAids = [];
+  if (props.aids) {
+    allAids = props.aids.filter(aid => aid.active === (props.filter ? props.filter.active: true))
+                      .filter(aid => props.filter && props.filter.admin ? aid.ambit === props.filter.admin : true)
+                      .filter(aid => props.filter && !aid.data_fi && aid.data_inici && getFilterTimeStamp(props.date) >= getAidTimeStamp(aid));
+    paginatedAids = allAids.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+  }
+
+  const exportPdf = () => {
+    const doc = new jsPDF()
+    doc.autoTable({
+      head: [['Codi', 'Descripció', 'Data inici','Data fi','Àmbit','Administració','Activa']],
+      body: allAids.map((aid) => [aid.codi, aid.descripcio, aid.data_inici, aid.data_fi, aid.tipus, aid.ambit, aid.active ? 'Si' : 'No']),
+      /*didDrawCell: (data) => {
+        if (data.section === 'body' && data.column.index === 6) {
+          var base64Img = 'data:image/jpeg;base64,iVBORw0KGgoAAAANS...'
+          doc.addImage(base64Img, 'JPEG', data.cell.x, data.cell.y, 10, 10)
+        }
+      },*/
+    })
+
+    doc.save('Ajudes.pdf')
+  }
+
   return (
     <Paper elevation={2}>
       <TableContainer component={Paper}>
@@ -47,11 +77,7 @@ const AidsTable = (props: Props) => {
           </TableHead>
           <TableBody>
             {props.aids &&
-              props.aids.filter(aid => aid.active === (props.filter ? props.filter.active: true))
-                        .filter(aid => props.filter && props.filter.admin ? aid.ambit === props.filter.admin : true)
-                        .filter(aid => props.filter && !aid.data_fi && aid.data_inici && getFilterTimeStamp(props.date) >= getAidTimeStamp(aid))
-                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        .map((aid) => (
+              paginatedAids.map((aid) => (
               <TableRow key={aid.code}>
                 <TableCell component="th" scope="row">{aid.codi}</TableCell>
                 <TableCell component="th" scope="row">{aid.descripcio}</TableCell>
@@ -70,6 +96,15 @@ const AidsTable = (props: Props) => {
               </TableRow>
             }
           </TableBody>
+          <TableFooter>
+            <TableRow footer>
+              <TableCell scope="row" size="small" align="center" colSpan={7}>
+                <Button color="primary" size="small" onClick={() => exportPdf()} startIcon={<PictureAsPdfIcon />}>
+                  Descarregar
+                </Button>
+              </TableCell>
+            </TableRow>
+          </TableFooter>
         </Table>
       </TableContainer>
       <TablePagination
@@ -81,7 +116,9 @@ const AidsTable = (props: Props) => {
           onChangePage={handleChangePage}
           onChangeRowsPerPage={handleChangeRowsPerPage}
         />
+
       </Paper>
+
     );
 }
 
