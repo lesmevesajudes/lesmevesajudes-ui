670426a4a3e0305b8e80ee435b950d9fa9bd8c78
Aritz <aritz>
2020-12-16 09:59:07 +0100

    add matomo script

--- src/App.js
+++ src/App.js
@@ -27,6 +27,34 @@ const preloadedState = window.__PRELOADED_STATE__;
 const store = configureStore(preloadedState);
 
 class App extends Component {
+
+  initialize_matomo = () => {
+		{/* Matomo */}
+		var _paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//ajuntament.barcelona.cat/piwik/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', 'jmyEnPrlLD']);
+            var d=document,
+            g=d.createElement('script'),
+            s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript';
+            g.async=true;
+            g.defer=true;
+            g.src=u+'matomo.js';
+            s.parentNode.insertBefore(g,s);
+          })();
+
+    	{/* End Matomo Code */}
+	}
+
+	componentDidMount() {
+		this.initialize_matomo();
+	}
+
   render() {
     return (
       <Suspense fallback="">
