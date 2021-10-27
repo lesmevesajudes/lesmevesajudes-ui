e7b4d0048eeae91faa1f707bf46d3125736dface
Jordi Buj <jordi.buj@jamgo.coop>
2021-10-27 23:57:43 +0200

    chore: refactor benefits to allow sharing with results page and dashboard

--- src/results/PersonalBenefits.js
+++ src/results/PersonalBenefits.js
@@ -5,129 +5,14 @@ import {Trans, withTranslation} from 'react-i18next';
 import Moment from 'moment';
 import {styles} from "../styles/theme";
 import {Person} from '../persons/PersonTypes';
-import {newDate} from "../shared/dateUtils";
 import BenefitRow, {NoBenefitRow} from './BenefitRow';
+import {getBenefits} from "../shared/benefits";
 
-const possibleBenefits = [
-  {
-    ID: 'AE_230_mensual',
-    name: <Trans i18nKey='AE_230_mensual_title'>Fons extraordinari d’ajuts d’emergència social per a infants de 0 a
-      16 anys</Trans>,
-    periode: <Trans i18nKey='mes'>mes</Trans>,
-    amountText: <Trans i18nKey='maxim_6_mesos'>ajuda mensual, màxim 6 mesos</Trans>,
-    url: 'link_AE_230',
-    from: newDate(2021, 6, 1),
-    to: newDate(2021, 12, 31)
-  },
-  {
-    ID: 'AE_230_01_mensual',
-    name: <Trans i18nKey='AE_230_01_mensual_title'>Fons extraordinari d’ajuts d’emergència social per a infants de 0
-      a 16 anys per a famílies
-      monoparentals</Trans>,
-    periode: <Trans i18nKey='any'>any</Trans>,
-    url: 'link_AE_230',
-    from: newDate(2019, 4, 25),
-    to: newDate(2019, 5, 24)
-  },
-  {
-    ID: 'EG_233_mensual',
-    name: <Trans i18nKey='EG_233_mensual_title'>Beques menjador escolar de Barcelona</Trans>,
-    amountText: <Trans i18nKey='EG_233_mensual_amount'>Entre 3 - 6 €/dia</Trans>,
-    url: 'link_EG_233',
-    from: newDate(2021, 9, 6),
-    to: newDate(2021, 9, 17)
-  },
-  {
-    ID: 'TA_01_00_00',
-    name: <Trans i18nKey='TA_01_00_00_title'>T-16: Targeta de transport públic gratuïta per als nens i nenes de 4 a 16 anys.</Trans>,
-    amountText: <Trans i18nKey='TA_01_00_00_amount'>Bonificació valorada en 40 €/mes</Trans>,
-    url: 'link_TA_01_00_00',
-    from: undefined,
-    to: undefined,
-  },
-  {
-    ID: 'TA_02_00_00',
-    name: <Trans i18nKey='TA_02_00_00_title'>T-Usual bonificada: Targeta de transport amb un descompte aplicat sobre el preu de compra habitual.</Trans>,
-    amountText: <Trans i18nKey='TA_02_00_00_amount'>Bonificació valorada en 30,05 €/mes</Trans>,
-    url: 'link_TA_02_00_00',
-    from: undefined,
-    to: undefined,
-  },
-  {
-    ID: 'GE_051_04_mensual',
-    name: <Trans i18nKey='GE_051_04_mensual_title'>Renda activa d'inserció aturats de llarga durada</Trans>,
-    periode: <Trans i18nKey='mes'>mes</Trans>,
-    url: 'link_GE_051',
-    from: undefined,
-    to: undefined
-  },
-  {
-    ID: 'GE_051_01_mensual',
-    name: <Trans i18nKey='GE_051_01_mensual_title'>Renda activa d'inserció discapacitat 33%</Trans>,
-    periode: <Trans i18nKey='mes'>mes</Trans>,
-    url: 'link_GE_051'
-  },
-  {
-    ID: 'GE_051_02_mensual',
-    name: <Trans i18nKey='GE_051_02_mensual_title'>Renda activa d'inserció per a emigrants retornats</Trans>,
-    periode: <Trans i18nKey='mes'>mes</Trans>,
-    url: 'link_GE_051',
-    from: undefined,
-    to: undefined
-  },
-  {
-    ID: 'GE_051_03_mensual',
-    name: <Trans i18nKey='GE_051_03_mensual_title'>Renda activa d'inserció per a víctimes de violència de gènere o
-      domèstica</Trans>,
-    periode: <Trans i18nKey='mes'>mes</Trans>,
-    url: 'link_GE_051',
-    from: undefined,
-    to: undefined
-  },
-  {
-    ID: 'GG_270_mensual',
-    name: <Trans i18nKey='GG_270_mensual_title'>Renda garantida ciutadana</Trans>,
-    amountText: <Trans i18nKey='GG_270_import'>Import segons el nombre de membres de la unitat familiar</Trans>,
-    url: 'link_GG_270',
-    from: undefined,
-    to: undefined,
-  },
-  {
-    ID: 'GA_246_01',
-    name: <Trans i18nKey='GA_246_01_title'>Targeta rosa metropolitana</Trans>,
-    url: 'link_GA_246',
-    amountText: <Trans i18nKey='GA_246_01_import'>Gratuïta</Trans>,
-    from: undefined,
-    to: undefined
-  },
-  {
-    ID: 'GA_246_02',
-    name: <Trans i18nKey='GA_246_02_title'>Targeta rosa metropolitana</Trans>,
-    url: 'link_GA_246',
-    amountText: <Trans i18nKey='GA_246_02_import'>Reduïda</Trans>,
-    from: undefined,
-    to: undefined
-  },
-  {
-   ID: 'GA_234_01',
-   name: <Trans i18nKey='GA_234_01_title'>Vincles</Trans>,
-   url: 'link_GA_234',
-   amountText: <Trans i18nKey='GA_234_01_import'>Gratuïta</Trans>,
-   from: undefined,
-   to: undefined
-},
- {
-  ID: 'GA_234_02',
-  name: <Trans i18nKey='GA_234_02_title'>Vincles</Trans>,
-  url: 'link_GA_234',
-  amountText: <Trans i18nKey='GA_234_02_import'>Amb tauleta pròpia</Trans>,
-  from: undefined,
-  to: undefined
-}
-];
 const period = Moment().format('YYYY-MM');
 
 const PersonalBenefits = (props) => {
+
+  const possibleBenefits = getBenefits('personal');
 
   function hasAnyBenefit(personWithBenefits) {
     if (typeof personWithBenefits === 'undefined') return false; //TODO hackish. this happends due some inconsistency in the request processing. On
---
+++ src/shared/benefits.js
@@ -0,0 +1,193 @@
+import {compose, filter, map, propEq, reject, isNil} from 'ramda';
+import {newDate} from './dateUtils';
+import i18n from '../i18n';
+
+const benefits = [
+    {
+        type: 'personal',
+        ID: 'AE_230_mensual',
+        name: 'AE_230_mensual_title',
+        periode: 'mes',
+        amountText: 'maxim_6_mesos',
+        url: 'link_AE_230',
+        from: newDate(2021, 6, 1),
+        to: newDate(2021, 12, 31)
+    },
+    {
+        type: 'personal',
+        ID: 'AE_230_01_mensual',
+        name: 'AE_230_01_mensual_title',
+        periode: 'any',
+        url: 'link_AE_230',
+        from: newDate(2019, 4, 25),
+        to: newDate(2019, 5, 24)
+    },
+    {
+        type: 'personal',
+        ID: 'EG_233_mensual',
+        name: 'EG_233_mensual_title',
+        amountText: 'EG_233_mensual_amount',
+        url: 'link_EG_233',
+        from: newDate(2021, 9, 6),
+        to: newDate(2021, 9, 17)
+    },
+    {
+        type: 'personal',
+        ID: 'TA_01_00_00',
+        name: 'TA_01_00_00_title',
+        amountText: 'TA_01_00_00_amount',
+        url: 'link_TA_01_00_00',
+    },
+    {
+        type: 'personal',
+        ID: 'TA_02_00_00',
+        name: 'TA_02_00_00_title',
+        amountText: 'TA_02_00_00_amount',
+        url: 'link_TA_02_00_00',
+    },
+    {
+        type: 'personal',
+        ID: 'GE_051_04_mensual',
+        name: 'GE_051_04_mensual_title',
+        periode: 'mes',
+        url: 'link_GE_051',
+    },
+    {
+        type: 'personal',
+        ID: 'GE_051_01_mensual',
+        name: 'GE_051_01_mensual_title',
+        periode: 'mes',
+        url: 'link_GE_051',
+    },
+    {
+        type: 'personal',
+        ID: 'GE_051_02_mensual',
+        name: 'GE_051_02_mensual_title',
+        periode: 'mes',
+        url: 'link_GE_051',
+    },
+    {
+        type: 'personal',
+        ID: 'GE_051_03_mensual',
+        name: 'GE_051_03_mensual_title',
+        periode: 'mes',
+        url: 'link_GE_051',
+    },
+    {
+        type: 'personal',
+        ID: 'GG_270_mensual',
+        name: 'GG_270_mensual_title',
+        amountText: 'GG_270_import',
+        url: 'link_GG_270',
+    },
+    {
+        type: 'personal',
+        ID: 'GA_246_01',
+        name: 'GA_246_01_title',
+        url: 'link_GA_246',
+        amountText: 'GA_246_01_import',
+    },
+    {
+        type: 'personal',
+        ID: 'GA_246_02',
+        name: 'GA_246_02_title',
+        url: 'link_GA_246',
+        amountText: 'GA_246_02_import',
+    },
+    {
+        type: 'personal',
+        ID: 'GA_234_01',
+        name: 'GA_234_01_title',
+        url: 'link_GA_234',
+        amountText: 'GA_234_01_import',
+    },
+    {
+        type: 'personal',
+        ID: 'GA_234_02',
+        name: 'GA_234_02_title',
+        url: 'link_GA_234',
+        amountText: 'GA_234_02_import',
+    },
+    {
+        type: 'household',
+        ID: 'HA_077_01',
+        name: 'HA_077_01_title',
+        url: 'link_HA_077_01',
+        amountText: 'HA_077_01_import',
+        conditions: 'maxim_12_mesos',
+        from: newDate(2019, 2, 20),
+        to: newDate(2019, 12, 5)
+    },
+    {
+        type: 'household',
+        ID: 'HG_077_02',
+        name: 'HG_077_02_title',
+        amountText: 'HG_077_02_import',
+        conditions: 'maxim_12_mesos',
+        url: 'link_HG_077_02',
+    },
+    {
+        type: 'household',
+        ID: 'HG_077_03',
+        name: 'HG_077_03_title',
+        amountText: 'HG_077_03_import',
+        conditions: 'pagament_unic',
+        url: 'link_HG_077_03',
+    },
+    {
+        type: 'household',
+        ID: 'HG_077_04',
+        name: 'HG_077_04_title',
+        amountText: 'HG_077_04_import',
+        conditions: 'pagament_unic',
+        url: 'link_HG_077_04',
+    },
+    {
+        type: 'household',
+        ID: 'HG_077_04_01',
+        name: 'HG_077_04_01_title',
+        amountText: 'HG_077_04_01_import',
+        conditions: 'maxim_12_mesos',
+        url: 'link_HG_077_04',
+    },
+    {
+        type: 'household',
+        ID: 'HE_077_00',
+        name: 'HE_077_00_title',
+        amountText: 'HE_077_00_import',
+        conditions: 'maxim_12_mesos',
+        url: 'link_HE_077_00',
+        from: newDate(2021, 4, 27),
+        to: newDate(2021, 6, 11)
+    },
+    {
+        type: 'household',
+        ID: 'HE_02_01_00',
+        name: 'HE_02_01_00_title',
+        url: 'link_HE_02_01_00',
+        amountText: 'HE_02_01_00_import',
+        to: newDate(2021, 4, 30)
+    },
+    {
+        type: 'household',
+        ID: 'HG_02_00_00',
+        name: 'HG_02_00_00_title',
+        url: 'link_HG_02_00_00',
+        amountText: 'HG_02_00_00_import',
+        conditions: 'maxim_12_mesos',
+    }
+];
+
+const translate = ({ name, amountText, conditions, periode, ...rest }) => ({
+    name: i18n.t(name),
+    amountText: i18n.t(amountText),
+    periode: i18n.t(periode),
+    conditions: i18n.t(conditions),
+    ...rest,
+});
+
+export const getBenefits = (type) => compose(
+    map(reject(isNil)),
+    map(translate),
+    filter(propEq('type', type)),
+)(benefits);
